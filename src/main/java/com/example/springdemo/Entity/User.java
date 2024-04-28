5f9a379c717996ff9a76500e1f525ed8ff635cbe
Huanghe Zhou <2371282455@qq.com>
2024-04-28 18:57:20 +0800

    Initial

---
+++ src/main/java/com/example/springdemo/Entity/User.java
@@ -0,0 +1,11 @@
+package com.example.springdemo.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User {
+    String name;
+    int age;
+}
