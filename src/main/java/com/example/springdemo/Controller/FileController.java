1f9c7ee51cb706c64c575a1f4bc0cc0c1c36dd0f
Huanghe Zhou <2371282455@qq.com>
2024-05-01 21:15:08 +0800

    实现阿里云OSS上传文件功能

--- src/main/java/com/example/springdemo/Controller/FileController.java
+++ src/main/java/com/example/springdemo/Controller/FileController.java
@@ -1,6 +1,9 @@
 package com.example.springdemo.Controller;
 
+import com.aliyuncs.exceptions.ClientException;
+import com.example.springdemo.Utilis.AliOSSUtils;
 import com.example.springdemo.Utilis.Result;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,13 +12,12 @@ import java.io.File;
 import java.io.IOException;
 
 @RestController
-
 public class FileController {
+    @Autowired
+    AliOSSUtils aliOss;
     @PostMapping("/upload")
-    public Result upload(MultipartFile image) throws IOException {
-        String filename = image.getOriginalFilename();
-        image.transferTo(new File("D:\\Code\\SpringDemo\\src\\main\\resources\\imgs\\" + filename));
-
-        return Result.success();
+    public Result upload(MultipartFile image) throws IOException, ClientException {
+        String url = aliOss.upload(image);
+        return Result.success(url);
     }
 }
